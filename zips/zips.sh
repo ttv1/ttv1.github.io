a6cd76704d224dba94eaf2e1ca1f37138a7f9c8c
Tim Menzies <tim@menzies.us>
2016-12-19 17:54:48 -0500

    saving

--- zips/zips.sh
+++ zips/zips.sh
@@ -1,76 +1,76 @@
- [ "table.py" -nt "table.html" ] && pycco table.py
+ [ "table.py" -nt "table.html" ] && pycco -d . table.py
 zip -qu zips/table.zip table.html pycco.css table.py $s.html num.py $s.html GLOBALS.py $s.html opt.py $s.html sym.py $s.html
 git add * zips/*
 
- [ "csv.py" -nt "csv.html" ] && pycco csv.py
+ [ "csv.py" -nt "csv.html" ] && pycco -d . csv.py
 zip -qu zips/csv.zip csv.html pycco.css csv.py $s.html
 git add * zips/*
 
- [ "cliffsDeltaeg.py" -nt "cliffsDeltaeg.html" ] && pycco cliffsDeltaeg.py
+ [ "cliffsDeltaeg.py" -nt "cliffsDeltaeg.html" ] && pycco -d . cliffsDeltaeg.py
 zip -qu zips/cliffsDeltaeg.zip cliffsDeltaeg.html pycco.css cliffsDeltaeg.py $s.html eg.py $s.html
 git add * zips/*
 
- [ "opt.py" -nt "opt.html" ] && pycco opt.py
+ [ "opt.py" -nt "opt.html" ] && pycco -d . opt.py
 zip -qu zips/opt.zip opt.html pycco.css opt.py $s.html
 git add * zips/*
 
- [ "bootstrap.py" -nt "bootstrap.html" ] && pycco bootstrap.py
+ [ "bootstrap.py" -nt "bootstrap.html" ] && pycco -d . bootstrap.py
 zip -qu zips/bootstrap.zip bootstrap.html pycco.css bootstrap.py $s.html
 git add * zips/*
 
- [ "sampleeg.py" -nt "sampleeg.html" ] && pycco sampleeg.py
+ [ "sampleeg.py" -nt "sampleeg.html" ] && pycco -d . sampleeg.py
 zip -qu zips/sampleeg.zip sampleeg.html pycco.css sampleeg.py $s.html thing.py $s.html num.py $s.html GLOBALS.py $s.html opt.py $s.html numbers.py $s.html sample.py $s.html bootstrap.py $s.html sym.py $s.html eg.py $s.html
 git add * zips/*
 
- [ "ranges.py" -nt "ranges.html" ] && pycco ranges.py
+ [ "ranges.py" -nt "ranges.html" ] && pycco -d . ranges.py
 zip -qu zips/ranges.zip ranges.html pycco.css ranges.py $s.html
 git add * zips/*
 
- [ "thing.py" -nt "thing.html" ] && pycco thing.py
+ [ "thing.py" -nt "thing.html" ] && pycco -d . thing.py
 zip -qu zips/thing.zip thing.html pycco.css thing.py $s.html num.py $s.html GLOBALS.py $s.html opt.py $s.html numbers.py $s.html sample.py $s.html bootstrap.py $s.html sym.py $s.html
 git add * zips/*
 
- [ "rangeseg.py" -nt "rangeseg.html" ] && pycco rangeseg.py
+ [ "rangeseg.py" -nt "rangeseg.html" ] && pycco -d . rangeseg.py
 zip -qu zips/rangeseg.zip rangeseg.html pycco.css rangeseg.py $s.html ranges.py $s.html eg.py $s.html
 git add * zips/*
 
- [ "egeg.py" -nt "egeg.html" ] && pycco egeg.py
+ [ "egeg.py" -nt "egeg.html" ] && pycco -d . egeg.py
 zip -qu zips/egeg.zip egeg.html pycco.css egeg.py $s.html eg.py $s.html
 git add * zips/*
 
- [ "src.py" -nt "src.html" ] && pycco src.py
+ [ "src.py" -nt "src.html" ] && pycco -d . src.py
 zip -qu zips/src.zip src.html pycco.css src.py $s.html GLOBALS.py $s.html opt.py $s.html
 git add * zips/*
 
- [ "num.py" -nt "num.html" ] && pycco num.py
+ [ "num.py" -nt "num.html" ] && pycco -d . num.py
 zip -qu zips/num.zip num.html pycco.css num.py $s.html GLOBALS.py $s.html opt.py $s.html
 git add * zips/*
 
- [ "eg.py" -nt "eg.html" ] && pycco eg.py
+ [ "eg.py" -nt "eg.html" ] && pycco -d . eg.py
 zip -qu zips/eg.zip eg.html pycco.css eg.py $s.html
 git add * zips/*
 
- [ "GLOBALS.py" -nt "GLOBALS.html" ] && pycco GLOBALS.py
+ [ "GLOBALS.py" -nt "GLOBALS.html" ] && pycco -d . GLOBALS.py
 zip -qu zips/GLOBALS.zip GLOBALS.html pycco.css GLOBALS.py $s.html opt.py $s.html
 git add * zips/*
 
- [ "sandbox.py" -nt "sandbox.html" ] && pycco sandbox.py
+ [ "sandbox.py" -nt "sandbox.html" ] && pycco -d . sandbox.py
 zip -qu zips/sandbox.zip sandbox.html pycco.css sandbox.py $s.html
 git add * zips/*
 
- [ "numeg.py" -nt "numeg.html" ] && pycco numeg.py
+ [ "numeg.py" -nt "numeg.html" ] && pycco -d . numeg.py
 zip -qu zips/numeg.zip numeg.html pycco.css numeg.py $s.html thing.py $s.html num.py $s.html GLOBALS.py $s.html opt.py $s.html numbers.py $s.html sample.py $s.html bootstrap.py $s.html sym.py $s.html eg.py $s.html
 git add * zips/*
 
- [ "numbers.py" -nt "numbers.html" ] && pycco numbers.py
+ [ "numbers.py" -nt "numbers.html" ] && pycco -d . numbers.py
 zip -qu zips/numbers.zip numbers.html pycco.css numbers.py $s.html GLOBALS.py $s.html opt.py $s.html
 git add * zips/*
 
- [ "sym.py" -nt "sym.html" ] && pycco sym.py
+ [ "sym.py" -nt "sym.html" ] && pycco -d . sym.py
 zip -qu zips/sym.zip sym.html pycco.css sym.py $s.html
 git add * zips/*
 
- [ "sample.py" -nt "sample.html" ] && pycco sample.py
+ [ "sample.py" -nt "sample.html" ] && pycco -d . sample.py
 zip -qu zips/sample.zip sample.html pycco.css sample.py $s.html bootstrap.py $s.html num.py $s.html GLOBALS.py $s.html opt.py $s.html numbers.py $s.html
 git add * zips/*
 
